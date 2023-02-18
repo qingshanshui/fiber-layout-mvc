1facb01853bf3a72f5b44cd579d63998e2b64297
青山水 <1578347363@qq.com>
2023-02-18 19:01:44 +0800

    优化 管理页面

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	v1 "fiber-layout-mvc/controllers/v1"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,6 +11,6 @@ func SetRoute(app *fiber.App) {
 	group := app.Group("/")
 	group.Get("/", main.Home)             // 首页
 	group.Get("/category", main.Category) // 详情
-	group.Get("/login",main.Login) // 登录
-	group.Get("/admin",main.Admin) // 管理页
+	group.Get("/login", main.Login)       // 登录
+	group.Get("/admin", main.Admin)       // 管理页
 }
