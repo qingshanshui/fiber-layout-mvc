a9e76561aa906e04b13f2cb9251132c4e4fbe811
青山水 <1578347363@qq.com>
2023-02-25 17:17:32 +0800

    自定义 参数错误处理

--- validator/form/default.go
+++ validator/form/default.go
@@ -2,5 +2,5 @@ package form
 
 // CategoryRequest 详情
 type CategoryRequest struct {
-	Id string `form:"id" json:"id"  validate:"required"` //  验证规则：必填，最小长度为5
+	Id string `form:"id" json:"id"  validate:"required,min=5" validate_error:"id不能为空"` //  验证规则：必填，最小长度为5
 }
--- validator/validator.go
+++ validator/validator.go
@@ -1,6 +1,10 @@
 package validator
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
@@ -32,7 +36,39 @@ func validateStruct(obj interface{}) error {
 	valid := validator.New()
 	err := valid.Struct(obj)
 	if err != nil {
-		return err
+		return errors.New(ProcessErr(obj, err))
 	}
 	return nil
+}
+
+func ProcessErr(u interface{}, err error) string {
+	if err == nil { //如果为nil 说明校验通过
+		return ""
+	}
+	invalid, ok := err.(*validator.InvalidValidationError) //如果是输入参数无效，则直接返回输入参数错误
+	if ok {
+		return "输入参数错误：" + invalid.Error()
+	}
+	validationErrs := err.(validator.ValidationErrors) //断言是ValidationErrors
+	for _, validationErr := range validationErrs {
+		fieldName := validationErr.Field() //获取是哪个字段不符合格式
+		typeOf := reflect.TypeOf(u)
+		// 如果是指针，获取其属性
+		if typeOf.Kind() == reflect.Ptr {
+			typeOf = typeOf.Elem()
+		}
+		field, ok := typeOf.FieldByName(fieldName) //通过反射获取filed
+		fmt.Println(field, ok, "field,ok")
+		if ok {
+			errorInfo := field.Tag.Get("validate_error") // 获取field对应的validate_error tag值
+			if errorInfo != "" {
+				return fieldName + ":" + errorInfo // 返回错误
+			} else {
+				return "缺失validate_error"
+			}
+		} else {
+			return "系统错误：获取不到反射体"
+		}
+	}
+	return ""
 }
