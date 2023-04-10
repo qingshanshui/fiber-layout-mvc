54acc2674ac478b3bac6d89d4f0a39ea37af1278
青山水 <1578347363@qq.com>
2023-04-10 14:59:51 +0800

    优化utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,7 +5,11 @@ import (
 	"time"
 )
 
-// StringBytesBufferJoin : 拼接字符串 bytes.Buffer模式
+/*StringBytesBufferJoin
+ * @description: 拼接字符串 bytes.Buffer模式
+ * @param {...string} con
+ * @return {*}
+ */
 func StringBytesBufferJoin(con ...string) string {
 	stringBytesBuffer := bytes.Buffer{}
 	for _, s := range con {
@@ -14,25 +18,37 @@ func StringBytesBufferJoin(con ...string) string {
 	return stringBytesBuffer.String()
 }
 
-// GetDayTime 返回当前的时间 :		2022-07-27 09:06:05
+/*GetDayTime
+ * @description: 返回当前的时间
+ * @return {*}
+ */
 func GetDayTime() string {
 	//template := "2006:01:02"
 	template := "2006-01-02 15:04:05" // 标准模板
 	return time.Now().Format(template)
 }
 
-// GetDay 获取当前年月日
+/*GetDay
+ * @description: 获取当前年月日
+ * @return {*}
+ */
 func GetDay() string {
 	template := "20060102"
 	return time.Now().Format(template)
 }
 
-// GetUnixNano 获取当前时间戳纳秒
+/*GetUnixNano
+ * @description: 获取当前时间戳纳秒
+ * @return {*}
+ */
 func GetUnixNano() int64 {
 	return time.Now().UnixNano()
 }
 
-// GetFileDay 获取当前年/月/日
+/*GetFileDay
+ * @description: 获取当前年/月/日
+ * @return {*}
+ */
 func GetFileDay() string {
 	template := "2006/01/02"
 	return time.Now().Format(template)
